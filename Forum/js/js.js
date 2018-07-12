838c3891dab0dc5507542473459bb7d21f453e9e
rouaix <daniel@rouaix.com>
2018-07-12 17:14:39 +0200

    Add files via upload

--- Forum/js/js.js
+++ Forum/js/js.js
@@ -1,3 +1,20 @@
 function setFocusSaisie(){
     document.getElementById("lemessage").focus();
 }
+
+function toggleEditeur()
+{
+	node = document.getElementById("editeur");
+	if (node.style.visibility=="hidden")
+	{
+		// Contenu caché, le montrer
+		node.style.visibility = "visible";
+		node.style.height = "100%";			// Optionnel rétablir la hauteur
+	}
+	else
+	{
+		// Contenu visible, le cacher
+		node.style.visibility = "hidden";
+		node.style.height = "0";			// Optionnel libérer l'espace
+	}
+}
